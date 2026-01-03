3ada37a45c73f37752251f1d347179bd549e130a
NaingWinAung <naingwinaung1710@gmail.com>
2026-01-03 13:56:54 +0700

    add additional option in show

--- app/Http/Resources/Admin/Product/ProductOptionBoatResource.php
+++ app/Http/Resources/Admin/Product/ProductOptionBoatResource.php
@@ -15,14 +15,24 @@ class ProductOptionBoatResource extends JsonResource
     public function toArray(Request $request) : array
     {
         return [
-            'id'             => $this->id,
-            'product_id'     => $this->product_id,
-            'boat_id'        => $this->boat_id,
-            'boat'           => $this->boat,
-            'start_date'     => $this->start_date,
-            'end_date'       => $this->end_date,
-            'schedule_times' => $this->scheduleTimes,
-            'tickets'        => $this->tickets,
+            'id'                 => $this->id,
+            'product_id'         => $this->product_id,
+            'boat_id'            => $this->boat_id,
+            'boat'               => $this->boat,
+            'start_date'         => $this->start_date,
+            'end_date'           => $this->end_date,
+            'schedule_times'     => $this->scheduleTimes,
+            'tickets'            => $this->tickets,
+            'additional_options' => $this->productAdditionalOptions->map(function ($option) {
+                return [
+                    'id'            => $option->id,
+                    'option'        => [
+                        'id' => $option->additionalOption->id,
+                    ],
+                    'selling_price' => $option->selling_price,
+                    'net_price'     => $option->net_price,
+                ];
+            }),
         ];
     }
 }
